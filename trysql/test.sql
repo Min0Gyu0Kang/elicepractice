8b8b1b8d8bdc5211396bd542b10de782a43d677d
AI 10기 강민규 <myname@g.skku.edu>
2024-05-21 22:50:19 +0900

    successfully viewed shared_kickboard

--- trysql/test.sql
+++ trysql/test.sql
@@ -1,16 +1,6 @@
-
-CREATE DATABASE shared_kickboard;
+-- Active: 1716297702905@@elice@3306
 
 SHOW DATABASES;
-
 USE shared_kickboard;
-
--- DROP DATABASE shared_kickboard;
-
-CREATE USER elice@localhost IDENTIFIED BY '0110';
-
-GRANT ALL PRIVILEGES ON shared_kickboard TO elice@localhost;`
-
-FLUSH PRIVILEGES;
-
-SHOW GRANTS FOR elice@localhost;
+SHOW TABLES; 
+DESC customer;
